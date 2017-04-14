453ebcc8887f1eee0b0aaa08f70864d04162bfa0
Branden Soropia <b.soropia@gmail.com>
2017-04-14 00:50:10 -0400

    Prototype off-canvas navigation. Placeholder 'Download' resume button.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
-// var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify');
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
@@ -27,18 +27,18 @@ gulp.task('minify-css', ['less'], function() {
 });
 
 // Minify JS
-// gulp.task('minify-js', function() {
-//     return gulp.src('js/new-age.js')
-//         .pipe(uglify())
-//         .pipe(rename({ suffix: '.min' }))
-//         .pipe(gulp.dest('js'))
-//         .pipe(browserSync.reload({
-//             stream: true
-//         }))
-// });
+gulp.task('minify-js', function() {
+    return gulp.src('js/off-canvas.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
 
 // Run everything
-gulp.task('default', ['less', 'minify-css']);
+gulp.task('default', ['less', 'minify-css', 'minify-js']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -50,11 +50,11 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css'], function() {
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
-    // gulp.watch('js/*.js', ['minify-js']);
+    gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
-    // gulp.watch('js/**/*.js', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
 });
---
+++ js/off-canvas.js
@@ -0,0 +1,9 @@
+function openNav () {
+    document.getElementById("side-nav").style.width = "100px";
+    document.getElementById("content").style.marginLeft = "100px";
+}
+
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+    document.getElementById("content").style.marginLeft = "0";
+}
